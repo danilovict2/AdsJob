197d5b2de1a5c062c31c4db993b9cf731e1b58e4
majstorbob212 <stanticdejo@gmail.com>
2023-02-01 23:08:15 +0100

    added profile dropdown menu

--- frontend/script.js
+++ frontend/script.js
@@ -32,5 +32,14 @@ function auth(){
     }
   }
 
+function dropDown() {
+    var click = document.getElementById("list-items");
+    if (click.style.display === "none") {
+       click.style.display = "block";
+    } else {
+       click.style.display = "none";
+    }
+ }
+
 
   
