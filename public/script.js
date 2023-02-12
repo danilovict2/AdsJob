93d660d9c2d1222f8ab66293c00fb6295c9a46d3
danilovict2 <danilosindric@gmail.com>
2023-02-12 22:40:39 +0100

    upd: started working on dropdown menu

--- public/script.js
+++ public/script.js
@@ -1,45 +1,64 @@
 function showPassword() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
   }
+}
 
 function navBarSetter() {
-    var navBar = document.getElementById("myTopnav");
-    if (navBar.className === "topnav") {
-      navBar.className += " responsive";
-    } else {
-      navBar.className = "topnav";
-    }
+  var navBar = document.getElementById("myTopnav");
+  if (navBar.className === "topnav") {
+    navBar.className += " responsive";
+  } else {
+    navBar.className = "topnav";
+  }
+
+}
+
+function auth() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  if (email == "primer@gmail.com" && password == "primer123") {
+    window.location.href = "indx-profile.html";
+    alert("Uspesna prijava!");
 
   }
-  
-function auth(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if(email == "primer@gmail.com" && password == "primer123"){
-        window.location.href = "indx-profile.html";
-        alert("Uspesna prijava!");
-      
-    }
-    else{
-      alert("Netacne informacije! Pokusajte ponovo");
-      return;
+  else {
+    alert("Netacne informacije! Pokusajte ponovo");
+    return;
 
-    }
   }
+}
 
 function dropDown() {
-    var click = document.getElementById("list-items");
-    if (click.style.display === "none") {
-       click.style.display = "block";
-    } else {
-       click.style.display = "none";
+  var click = document.getElementById("list-items");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+
+function enableDropdown(){
+    if(typeof enableDropdown.isEnabled == 'undefined'){
+        enableDropdown.isEnabled = false;
     }
- }
+    enableDropdown.isEnabled = !enableDropdown.isEnabled;
+    var dropdownContent = document.getElementById("dropdown-content");
+    if(enableDropdown.isEnabled){
+      dropdownContent.style.display = "block";
+    }else{
+      dropdownContent.style.display = "none";
+    }
+}
 
-
+window.onclick = function(event){
+  if(!event.target.matches('.enableDropdown')){
+    if(enableDropdown.isEnabled){
+      document.getElementById("dropdown-content").style.display = "none";
+    }
+  }
   
+}
