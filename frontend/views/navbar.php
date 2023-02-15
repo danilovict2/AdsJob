5fabe1aae72427ff3ad0d35335419ff16b71f84d
danilovict2 <danilosindric@gmail.com>
2023-02-15 15:49:33 +0100

    udp: file organization

--- frontend/views/navbar.php
+++ frontend/views/navbar.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="search.css">
-    <link rel="stylesheet" href="dropdown.css">
-    <link rel="stylesheet" href="profile.css">
-    <link rel="stylesheet" href="findj.css">
-    <link rel="stylesheet" href="signup.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/search.css">
+    <link rel="stylesheet" href="../css/dropdown.css">
+    <link rel="stylesheet" href="../css/profile.css">
+    <link rel="stylesheet" href="../css/findj.css">
+    <link rel="stylesheet" href="../css/signup.css">
     <script src="../javascript/script.js"></script>
     <script src="../javascript/dropdown.js"></script>
     <title>Home</title>
