f20439a6d990671d3916fd04fe8fb5b90c927cc4
danilovict2 <danilosindric@gmail.com>
2023-02-20 20:06:24 +0100

    add: templating

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types = 1);
 
 namespace AdsJob\Controllers;
+
+use AdsJob\Template\Renderer;
 use Http\Response;
 use Http\Request;
 
@@ -8,17 +10,23 @@ class Homepage{
 
     private $request;
     private $response;
+    private $renderer;
 
-    public function __construct(Request $request, Response $response)
-    {
+    public function __construct(
+        Request $request, 
+        Response $response,
+        Renderer $renderer
+    ){
         $this->request = $request;
         $this->response = $response;
+        $this->renderer = $renderer;
     }
 
-    public function show()
-    {
-        $content = '<h1>Hello World</h1>';
-        $content .= 'Hello ' . $this->request->getParameter('name', 'stranger');
-        $this->response->setContent($content);
+    public function show(){
+        $data = [
+            'name' => $this->request->getParameter('name', 'stranger'),
+        ];
+        $html = $this->renderer->render('index', $data);
+        $this->response->setContent($html);
     }
 }
--- src/Dependencies.php
+++ src/Dependencies.php
@@ -15,4 +15,13 @@ $injector->define('Http\HttpRequest', [
 $injector->alias('Http\Response', 'Http\HttpResponse');
 $injector->share('Http\HttpResponse');
 
+$injector->alias('AdsJob\Template\Renderer', 'AdsJob\Template\MustacheRenderer');
+$injector->define('Mustache_Engine', [
+    ':options' => [
+        'loader' => new Mustache_Loader_FilesystemLoader(dirname(__DIR__) . '/frontend/views', [
+            'extension' => '.php',
+        ]),
+    ],
+]);
+
 return $injector;
