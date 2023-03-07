c0650b09ffc97091f0f6e2d62da2b0a383455e09
danilovict2 <danilosindric@gmail.com>
2023-03-07 20:13:43 +0100

    upd: added routes

--- src/Routes.php
+++ src/Routes.php
@@ -1,11 +1,15 @@
 <?php declare(strict_types = 1);
 
 return [
-    ['GET', '/', ['AdsJob\Controllers\FrontendController', 'index']], //renders views/index.php
-    ['GET', '/post-find-job', ['AdsJob\Controllers\FrontendController', 'showPostFindJob']], //renders views/post-find-job.php
-    ['GET', '/login', ['AdsJob\Controllers\FrontendController', 'showLogin']], //renders views/login.php
-    ['GET', '/register', ['AdsJob\Controllers\FrontendController', 'showRegister']], //renders views/register.php
-    ['GET', '/profile/{user_id}', ['AdsJob\Controllers\ProfileController', 'index']], //renders profile.php
+    ['GET', '/', ['AdsJob\Controllers\FrontendController', 'index']],
+    ['GET', '/post-find-job', ['AdsJob\Controllers\FrontendController', 'postFindJob']],
+    ['GET', '/login', ['AdsJob\Controllers\FrontendController', 'login']],
+    ['GET', '/register', ['AdsJob\Controllers\FrontendController', 'register']],
+    ['GET', '/profile/{user_id}', ['AdsJob\Controllers\ProfileController', 'index']],
+    ['GET', '/profile/{user_id}/comments', ['AdsJob\Controllers\ProfileController', 'comments']],
+    ['GET', '/p/create', ['AdsJob\Controllers\JobController', 'create']],
+    ['GET', '/profile/{user_id}/reviews', ['AdsJob\Controllers\ProfileController', 'reviews']],
+    ['GET', '/profile/{user_id}/edit', ['AdsJob\Controllers\ProfileController', 'edit']],
     ['GET', '/{slug}' , ['AdsJob\Controllers\PageController','show']],
 ];
 
