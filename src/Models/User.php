9a299c42127c68e468c7176abf65eb4fb08970c0
danilovict2 <danilosindric@gmail.com>
2023-03-23 20:03:16 +0100

    add: Models

---
+++ src/Models/User.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace AdsJob\Models;
+
+class User extends Model{
+
+    protected string $email, $firstName, $lastName, $password;
+
+    public function register(){
+
+    }
+}
