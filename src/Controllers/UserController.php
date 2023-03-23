4efbae903a8f4438a31baa4853fd7ec1fd608847
danilovict2 <danilosindric@gmail.com>
2023-03-23 20:03:40 +0100

    upd: started working on form validation through models

---
+++ src/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+namespace AdsJob\Controllers;
+use AdsJob\Models\User;
+
+class UserController extends Controller{
+
+    public function store(){
+        $user = new User();
+        $user->loadData($this->request->getBodyParameters());
+        $html = $this->renderer->render('index.html');
+        $this->response->setContent($html);
+    }
+}
