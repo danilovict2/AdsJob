90fe241f5636ed4cb469f62d028b1b81ac8c018a
danilovict2 <danilosindric@gmail.com>
2023-03-25 13:51:06 +0100

    upd: minor changes

--- src/Routes.php
+++ src/Routes.php
@@ -1,16 +1,16 @@
 <?php declare(strict_types = 1);
 
 return [
-    ['GET', '/', ['AdsJob\Controllers\FrontendController', 'index']],
-    ['GET', '/post-find-job', ['AdsJob\Controllers\FrontendController', 'postFindJob']],
-    ['GET', '/login', ['AdsJob\Controllers\FrontendController', 'login']],
-    ['GET', '/register', ['AdsJob\Controllers\FrontendController', 'register']],
-    ['GET', '/profile/{user_id}', ['AdsJob\Controllers\ProfileController', 'index']],
-    ['GET', '/profile/{user_id}/comments', ['AdsJob\Controllers\ProfileController', 'comments']],
-    ['GET', '/profile/{user_id}/reviews', ['AdsJob\Controllers\ProfileController', 'reviews']],
-    ['GET', '/profile/{user_id}/edit', ['AdsJob\Controllers\ProfileController', 'edit']],
-    ['GET', '/p/create', ['AdsJob\Controllers\JobController', 'create']],
-    ['GET', '/p/{job_id}', ['AdsJob\Controllers\JobController', 'show']],
-    ['POST', '/user/store', ['AdsJob\Controllers\UserController', 'store']],
+    ['GET', '/', [AdsJob\Controllers\FrontendController::class, 'index']],
+    ['GET', '/post-find-job', [AdsJob\Controllers\FrontendController::class, 'postFindJob']],
+    ['GET', '/login', [AdsJob\Controllers\FrontendController::class, 'login']],
+    ['GET', '/register', [AdsJob\Controllers\FrontendController::class, 'register']],
+    ['GET', '/profile/{user_id}', [AdsJob\Controllers\ProfileController::class, 'index']],
+    ['GET', '/profile/{user_id}/comments', [AdsJob\Controllers\ProfileController::class, 'comments']],
+    ['GET', '/profile/{user_id}/reviews', [AdsJob\Controllers\ProfileController::class, 'reviews']],
+    ['GET', '/profile/{user_id}/edit', [AdsJob\Controllers\ProfileController::class, 'edit']],
+    ['GET', '/p/create', [AdsJob\Controllers\JobController::class, 'create']],
+    ['GET', '/p/{job_id}', [AdsJob\Controllers\JobController::class, 'show']],
+    ['POST', '/user/store', [AdsJob\Controllers\UserController::class, 'store']],
 ];
 
