8a090bff90ce50cfe18128cd268751d58b3b3848
danilovict2 <danilosindric@gmail.com>
2023-04-14 22:44:51 +0200

    upd: updated routes

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -2,9 +2,14 @@
 
 namespace AdsJob\Controllers;
 use AdsJob\Models\User;
+use AdsJob\Middleware\AuthMiddleware;
 
 class UserController extends Controller{
 
+    public function middleware(){
+        $this->registerMiddleware(new AuthMiddleware($this->auth,['index']));
+    }
+    
     public function store() : void{
         $validator = new \AdsJob\Validators\Validator([
             'firstName' => ['required'],
@@ -22,6 +27,26 @@ class UserController extends Controller{
             $this->setValidationErrors($validator->getErrors());
             $this->response->redirect('/register');
         }
+    }
+
+    public function profile() : void{
+        $html = $this->renderer->render('profile.html',$this->requiredData);
+        $this->response->setContent($html);
+    }
+
+    public function myReviews() : void{
+        $html = $this->renderer->render('reviews.html',$this->requiredData);
+        $this->response->setContent($html);
+    }
+
+    public function editProfile() : void{
+        $html = $this->renderer->render('editProfile.html',$this->requiredData);
+        $this->response->setContent($html);
+    }
+
+    public function myJobs() : void{
+        $html = $this->renderer->render('myJobs.html', $this->requiredData);
+        $this->response->setContent($html);
     }
 
 }
