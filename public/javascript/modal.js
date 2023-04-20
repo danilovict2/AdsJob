0dd6b13182603ff32540e07fef8262293c1c3901
danilovict2 <danilosindric@gmail.com>
2023-04-20 21:27:28 +0200

    add: extracted modal displaying to separate file

---
+++ public/javascript/modal.js
@@ -0,0 +1,17 @@
+let modal = document.getElementById("modal");
+let button = document.getElementById("openModal");
+let closeButton = document.getElementById("close");
+
+button.onclick = function () {
+    modal.style.display = "block";
+}
+
+closeButton.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
