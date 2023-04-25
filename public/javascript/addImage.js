d4d0d8566d4d9335adafe486341f1da7735f8a1d
majstorbob212 <stanticdejo@gmail.com>
2023-04-25 16:42:48 +0200

    added js for images on post job

--- public/javascript/addImage.js
+++ public/javascript/addImage.js
@@ -1,8 +1,23 @@
-let images = document.querySelectorAll("input[type='file']");
+var previewImage = (event) => {
+    const imageFiles = event.target.files;
+    const imageFilesLength = imageFiles.length;
+    if (imageFilesLength > 0) {
+        const imageSrc = URL.createObjectURL(imageFiles[0]);
+        var imagePreviewElement = document.querySelector("#preview-selected-image");
+        imagePreviewElement.src = imageSrc;
 
-for(let image of images){
-    image.addEventListener("change", event =>{
-        let label = document.querySelector(`label[for='${image.id}']`);
-        label.classList.replace("input-images-box", "jobImage");
-    });
-}
+        imagePreviewElement.style.display = "block";
+    }
+};
+
+var previewImage2 = (event) => {
+    const imageFiles = event.target.files;
+    const imageFilesLength = imageFiles.length;
+    if (imageFilesLength > 0) {
+        const imageSrc = URL.createObjectURL(imageFiles[0]);
+        var imagePreviewElement = document.querySelector("#preview-selected-image2");
+        imagePreviewElement.src = imageSrc;
+
+        imagePreviewElement.style.display = "block";
+    }
+};
