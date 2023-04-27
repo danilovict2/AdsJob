5e6bff364abda2ca573e94de3148c7814021d96c
danilovict2 <danilosindric@gmail.com>
2023-04-27 22:36:57 +0200

    upd: implemented another way to close sidebar

--- public/javascript/navbar.js
+++ public/javascript/navbar.js
@@ -3,3 +3,10 @@ function enableSidebar(){
     const dropdownContent = dropdown.querySelector('.sidebar-content')
     dropdownContent.classList.toggle('sidebar-content-open');
 }
+
+window.addEventListener("click", event => {
+    if(!event.target.classList.contains('sidebarIcon') && !event.target.classList.contains('enableSidebar')){
+        const dropdown = document.querySelector('.sidebar-content');
+        dropdown.classList.remove("sidebar-content-open");
+    }
+});
